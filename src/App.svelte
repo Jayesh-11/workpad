9340fdac43373aee08db89f2d28edde839d134ad
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2026-09-01 17:39:00 +0530

    hero section update

--- src/App.svelte
+++ src/App.svelte
@@ -2,20 +2,17 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from './assets/vite.svg'
   import heroImg from './assets/hero.png'
-  import Counter from './lib/Counter.svelte'
+  import Features from './lib/Features.svelte'
 </script>
 
 <section id="center">
   <div class="hero">
-    <img src={heroImg} class="base" width="170" height="179" alt="" />
-    <img src={svelteLogo} class="framework" alt="Svelte logo" />
-    <img src={viteLogo} class="vite" alt="Vite logo" />
   </div>
   <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/App.svelte</code> and save to test <code>HMR</code></p>
+    <h1>Workpad</h1>
+    <p>Your private notes, snippets, and more, saved locally</p>
   </div>
-  <Counter />
+  <Features />
 </section>
 
 <div class="ticks"></div>
---
+++ src/lib/Features.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  // let count: number = $state(0)
+  // const increment = () => {
+  //   count += 1
+  // }
+</script>
+
+<div class="hero-features-container">
+  <ul class="hero-features">
+    <li>Fully Local</li>
+    <li>No Signup Required</li>
+    <li>No network calls</li>
+    <li>No data collection</li>
+    <li>No telemetry</li>
+  </ul>
+  <ul class="hero-features">
+    <li>Keyboard first</li>
+    <li>Fuzzy finder</li>
+    <li>Markdown support</li>
+    <li>Export your collection</li>
+    <li>Import your collection</li>
+  </ul>
+</div>
