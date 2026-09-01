d2709f381cf8633db3f4488df8498c4ecc76c3b3
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2026-09-02 04:35:12 +0530

    moved about to dialog

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,36 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from './assets/vite.svg'
-  import heroSvg from './assets/hero.svg'
-  import heroImg from './assets/hero.png'
-  import Features from './lib/Features.svelte'
+  import About from "./about/About.svelte";
 </script>
 
-<section id="center">
-  <div class="hero">
-    <div class="title"><img src={heroSvg} class="hero-svg" alt="Svelte logo" /><h1>Workpad</h1></div>
-    <p>Your private notes, snippets, and more, saved locally</p>
-  </div>
-  <Features />
-</section>
+<About />
 
-<div class="ticks"></div>
-
-<section id="next-steps">
-  <div id="social">
-    <div class="keyboard-shortcuts">
-      <p><kbd>⌘</kbd> + <kbd>/</kbd> - open keyboard shortcuts</p>
-      <p><kbd>⌘</kbd> + <kbd>K</kbd> - open the search bar</p>
-      <p><kbd>⌘</kbd> + <kbd>N</kbd> - create a new note</p>
-      <p><kbd>⌘</kbd> + <kbd>S</kbd> - save the current note</p>
-    </div>
-  </div>
-</section>
+<section id="center"></section>
 
 <div class="ticks"></div>
 <section id="spacer"></section>
 
 <section class="footer">
-  <a href="https://x.com/j0_dev" target="_blank" rel="noreferrer">
-    X
-  </a>
+  <a href="https://x.com/j0_dev" target="_blank" rel="noreferrer"> X </a>
   <span>•</span>
-  <a href="https://github.com/j0dev/workpad" target="_blank" rel="noreferrer">
+  <a
+    href="https://github.com/Jayesh-11/workpad"
+    target="_blank"
+    rel="noreferrer"
+  >
     GitHub
   </a>
   <span>•</span>
   <a href="https://j0dev.com/" target="_blank" rel="noreferrer">
     By Jayesh Vyavahare
   </a>
+  <span>•</span>
+  <a
+    href="https://github.com/Jayesh-11/workpad/releases"
+    target="_blank"
+    rel="noreferrer"
+  >
+    Changelog
+  </a>
+  <span>•</span>
+  <button command="show-modal" commandfor="about-dialog">About</button>
 </section>
---
+++ src/about/About.svelte
@@ -0,0 +1,31 @@
+<script>
+  import heroSvg from "../assets/hero.svg";
+  import Features from "../lib/Features.svelte";
+</script>
+
+<dialog id="about-dialog" class="about-dialog">
+  <section id="center">
+    <div class="hero">
+      <div class="title">
+        <img src={heroSvg} class="hero-svg" alt="Svelte logo" />
+        <h1>Workpad</h1>
+      </div>
+      <p>Your private notes, snippets, and more, saved locally</p>
+    </div>
+    <Features />
+  </section>
+
+  <div class="ticks"></div>
+
+  <section id="next-steps">
+    <div id="social">
+      <div class="keyboard-shortcuts">
+        <p><kbd>⌘</kbd> + <kbd>/</kbd> - open keyboard shortcuts</p>
+        <p><kbd>⌘</kbd> + <kbd>K</kbd> - open the search bar</p>
+        <p><kbd>⌘</kbd> + <kbd>U</kbd> - create a new note</p>
+        <p><kbd>⌘</kbd> + <kbd>S</kbd> - save the current note</p>
+      </div>
+    </div>
+  </section>
+  <button commandfor="about-dialog" command="close">Close</button>
+</dialog>
