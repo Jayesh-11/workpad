837da609a2c597f4cfba14e50cd5dc49ec74ef3f
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2026-09-04 23:51:41 +0530

    added tanstack form svelte

--- src/createSnippet/CreateSnippet.svelte
+++ src/createSnippet/CreateSnippet.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { workpadDB } from "../db/db.svelte";
   import { CREATE_SNIPPET_KEY } from "../consts/constants";
+  import { createForm } from "@tanstack/svelte-form";
+  import FieldInfo from "./FieldInfo.svelte";
 
   let dialog: HTMLDialogElement;
   document.addEventListener("keydown", (e) => {
@@ -11,6 +13,18 @@
       dialog.showModal();
     }
   });
+
+  const form = createForm(() => ({
+    defaultValues: {
+      name: "",
+      description: "",
+      isFavorite: false,
+    },
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      alert(JSON.stringify(value));
+    },
+  }));
 </script>
 
 <dialog
@@ -22,13 +36,142 @@
     <h2>Create Snippet</h2>
   </section>
 
-  <div class="ticks"></div>
-
   <section id="next-steps">
-    <input type="text" placeholder="Snippet name" />
-    <input type="text" placeholder="Snippet description" />
+    <form
+      id="form"
+      onsubmit={(e) => {
+        e.preventDefault();
+        e.stopPropagation();
+        form.handleSubmit();
+      }}
+    >
+      <form.Field
+        name="name"
+        validators={{
+          onChange: ({ value }) =>
+            value.length < 3 ? "Not long enough" : undefined,
+          onChangeAsyncDebounceMs: 500,
+          onChangeAsync: async ({ value }) => {
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+            return (
+              value.includes("error") && 'No "error" allowed in first name'
+            );
+          },
+        }}
+      >
+        {#snippet children(field)}
+          <div>
+            <label for={field.name}>First Name</label>
+            <input
+              id={field.name}
+              type="text"
+              placeholder="First Name"
+              value={field.state.value}
+              onblur={() => field.handleBlur()}
+              oninput={(e: Event) => {
+                const target = e.target as HTMLInputElement;
+                field.handleChange(target.value);
+              }}
+            />
+            <FieldInfo {field} />
+          </div>
+        {/snippet}
+      </form.Field>
+      <form.Field
+        name="description"
+        validators={{
+          onChange: ({ value }) =>
+            value.length < 3 ? "Not long enough" : undefined,
+        }}
+      >
+        {#snippet children(field)}
+          <div>
+            <label for={field.name}>Last Name</label>
+            <input
+              id={field.name}
+              type="text"
+              placeholder="Last Name"
+              value={field.state.value}
+              onblur={() => field.handleBlur()}
+              oninput={(e: Event) => {
+                const target = e.target as HTMLInputElement;
+                field.handleChange(target.value);
+              }}
+            />
+            <FieldInfo {field} />
+          </div>
+        {/snippet}
+      </form.Field>
+      <form.Field name="isFavorite">
+        {#snippet children(field)}
+          <div>
+            <label for={field.name}>Employed?</label>
+            <input
+              oninput={() => field.handleChange(!field.state.value)}
+              checked={field.state.value}
+              onblur={() => field.handleBlur()}
+              id={field.name}
+              type="checkbox"
+            />
+          </div>
+          {#if field.state.value}
+            <form.Field
+              name="description"
+              validators={{
+                onChange: ({ value }) =>
+                  value.length === 0
+                    ? "If you have a job, you need a title"
+                    : null,
+              }}
+            >
+              {#snippet children(field)}
+                <div>
+                  <label for={field.name}>Job Title</label>
+                  <input
+                    type="text"
+                    id={field.name}
+                    placeholder="Job Title"
+                    value={field.state.value}
+                    onblur={field.handleBlur}
+                    oninput={(e: Event) => {
+                      const target = e.target as HTMLInputElement;
+                      field.handleChange(target.value);
+                    }}
+                  />
+                  <FieldInfo {field} />
+                </div>
+              {/snippet}
+            </form.Field>
+          {/if}
+        {/snippet}
+      </form.Field>
+      <div>
+        <form.Subscribe
+          selector={(state) => ({
+            canSubmit: state.canSubmit,
+            isSubmitting: state.isSubmitting,
+          })}
+        >
+          {#snippet children({ canSubmit, isSubmitting })}
+            <button type="submit" disabled={!canSubmit}>
+              {isSubmitting ? "Submitting" : "Submit"}
+            </button>
+          {/snippet}
+        </form.Subscribe>
+        <button
+          type="button"
+          id="reset"
+          onclick={() => {
+            form.reset();
+          }}
+        >
+          Reset
+        </button>
+      </div>
+    </form>
   </section>
 
+  <div class="ticks"></div>
   <section class="about-dialog-footer">
     <p><kbd>Esc</kbd> - Close the dialog</p>
   </section>
