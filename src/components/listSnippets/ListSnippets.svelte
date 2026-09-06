e8d5cb6bb74e69aad85c94f0832606b525a4372e
Jayesh-11 <jayeshvyavahare132000@gmail.com>
2026-09-06 20:46:08 +0530

    added some basic styles

--- src/components/listSnippets/ListSnippets.svelte
+++ src/components/listSnippets/ListSnippets.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
   import transactions from "../../db/transactions.svelte";
   import { generateText, generateHTML } from "@tiptap/core";
-  import "./ListSnippets.css";
   import MiniSearch from "minisearch";
   import { StarterKit } from "@tiptap/starter-kit";
 
+  interface Snippet {
+    id: string;
+    rawData: string;
+    data: string;
+    createdAt: string;
+    updatedAt: string;
+  }
+
   const miniSearch = new MiniSearch({
     fields: ["id", "data"],
-    storeFields: ["id", "rawData"],
+    storeFields: ["id", "rawData", "createdAt", "updatedAt"],
   });
 
-  let snippets = $state([]);
+  let snippets = $state<Snippet[]>([]);
   let error = $state(null);
   let query = $state("");
   let rawSnippetsTransaction = transactions.getSnippets();
@@ -30,6 +37,7 @@
         });
         miniSearch.addAll(snippets);
       }
+      console.log("snippets", snippets);
     };
 
     if (rawSnippetsTransaction?.onerror) {
@@ -46,17 +54,46 @@
 </script>
 
 <section id="center">
-  <input type="text" placeholder="Search" bind:value={query} />
+  <input
+    class="search-input"
+    type="text"
+    placeholder="Search"
+    bind:value={query}
+  />
   <section class="list-snippets">
     {#if snippets.length === 0}
       <p>No snippets yet.</p>
     {:else}
       {#each results as snippet}
         <div class="snippet-card">
-          <h3>{snippet.id}</h3>
           <div>{@html snippet.rawData}</div>
         </div>
       {/each}
     {/if}
   </section>
 </section>
+
+<style>
+  .list-snippets {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    flex-wrap: wrap;
+    max-width: 850px;
+  }
+
+  .snippet-card {
+    border: 1px solid #ccc;
+    padding: 10px;
+    margin: 10px;
+    border-radius: 0.5rem;
+    width: 350px;
+  }
+
+  .search-input {
+    padding: 0.5rem;
+    border: 1px solid #ccc;
+    border-radius: 0.5rem;
+    margin-bottom: 1rem;
+  }
+</style>
